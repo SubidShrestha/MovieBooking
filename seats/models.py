98736828e4840249adc4409f626a851b0b4c0914
SubidShrestha <subideminem@gmail.com>
2022-08-15 21:58:49 +0545

    Seats and Booked Seats Model

---
+++ seats/models.py
@@ -0,0 +1,24 @@
+from django.db import models
+from users.models import user
+from movies.models import movie
+
+class seat(models.Model):
+    seat_code=models.CharField(max_length=5,null=True)
+    booking=models.BooleanField(default=False)
+    price=models.IntegerField(default=200)
+    updated_at=models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.seat_code
+
+class bookedseat(models.Model):
+    seat=models.ForeignKey(seat,on_delete=models.CASCADE,null=True)
+    movie=models.ForeignKey(movie,on_delete=models.CASCADE,null=True)
+    customer=models.ForeignKey(user,on_delete=models.CASCADE,null=True)
+    movie_date=models.DateField(null=True)
+    movie_time=models.TimeField(null=True)
+    booked_date=models.DateField(auto_now_add=True)
+    booked_time=models.TimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.seat.seat_code
